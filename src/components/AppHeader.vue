8b284d38500d42d0f41c84f618051e898a66bc16
rrd <rrd@1108.cc>
2021-05-22 15:14:46 +0200

    prettier fixes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,19 +1,25 @@
 <template>
   <header class="row">
     <div class="column">
-      <a href="https://kerdoiv.krisnavolgy.hu"><img src="../assets/logo.png" /></a>
+      <a href="https://kerdoiv.krisnavolgy.hu"
+        ><img src="../assets/logo.png"
+      /></a>
     </div>
     <nav class="column">
-      <h1>Kérdőív<span v-show="url != '/admin'"> {{ this.$store.state.step + 1 }} / 8</span></h1>
+      <h1>
+        Kérdőív<span v-show="url != '/admin'">
+          {{ this.$store.state.step + 1 }} / 8</span
+        >
+      </h1>
     </nav>
   </header>
 </template>
 
 <script>
 export default {
-  name: "AppHeader",
-  props: ['url']
-};
+  name: 'AppHeader',
+  props: ['url'],
+}
 </script>
 
 <style scoped>
@@ -25,7 +31,7 @@ header img {
   height: 10vh;
 }
 h1 {
-  font-family: "Quicksand", sans-serif;
+  font-family: 'Quicksand', sans-serif;
 }
 nav {
   font-size: 1.2rem;
